38f9652ca14c28c4e98fba8a4831a0162945615d
Heinrich Schmidt <littlepervert@yandex.ru>
2021-12-24 15:18:10 +0300

    Нижние модельки

---
+++ components/constructor3d/models/boxes/constants.js
@@ -0,0 +1,37 @@
+export const constants = () => {
+  const sideDepth = .16;
+  const legsHeight = 1;
+  const legsRad = 0.3;
+  const legFrontMargin = 0.38;
+  const sideTop = 0.8;
+  const scale = 1
+
+  const drawerTopGap = 0.48; // верхний отступ выдвижного ящика
+  const drawerTopGapBig = 0.64; // верхний бОльший отступ выдвижного ящика
+  const drawerSideGap = 0.15;  //боковой отступ выдвижного ящика
+  const drawerBottomGap = 0.12;  //нижний отступ выдвижного ящика
+  const drawerSideDepth = 0.02; // толщина стенки выдвижного ящика
+  const drawerBetweenGap = 0.94; 
+
+  const bottomHeight = 8.2
+  const bottomDepth = 4.8
+
+  return {
+    sideDepth,
+    legsHeight,
+    legsRad,
+    legFrontMargin,
+    sideTop,
+    scale,
+    bottomHeight,
+    bottomDepth,
+    drawerSideGap,
+    drawerBottomGap,
+    drawerSideDepth,
+    drawerTopGap,
+    drawerTopGapBig,
+    drawerBetweenGap
+
+  }
+}
+
--- components/constructor3d/models/boxes/f_400.js
+++ components/constructor3d/models/boxes/f_400.js
@@ -1,21 +1,24 @@
+import {constants}  from "./constants";
 import { bottomBox } from "./BottomBox"
+import { bottomBeams } from "./BottomBeams"
 import {BoxGeometry, Mesh, Group, QuaternionKeyframeTrack, Quaternion, Vector3, AnimationClip, AnimationMixer, AnimationClipCreator} from "three";
 
 import Materials from "../Materials";
 const { defaultMaterial } = Materials
 
 const width = 4
-const height = 8.2
-const depth = 4.8
 
-const sideDepth = .16;
-const legsHeight = 1;
-const sideTop = 0.8;
+const height = constants().bottomHeight
+const depth = constants().bottomDepth
+
+const sideDepth = constants().sideDepth;
+const legsHeight = constants().legsHeight;
+const scale = constants().scale
+
 const sideY = (height - legsHeight) / 2 - sideDepth
 
-const scale = 1
-
 export const f_400 = () => {
+  const beams = bottomBeams(width)
   const wrap = bottomBox(width, height, depth)
   const boxGroup = wrap.boxGroup
   const caseGroup = wrap.caseGroup
@@ -23,30 +26,26 @@ export const f_400 = () => {
 
   const sideGeometry = new BoxGeometry(sideDepth, height - legsHeight, depth);
   const sideBackGeometry = new BoxGeometry(width - sideDepth * 2, height - legsHeight, sideDepth);
-  const sideTopGeometry = new BoxGeometry(width - sideDepth * 2, sideDepth, sideTop);
   const shelfGeometry = new BoxGeometry(width - sideDepth * 2, sideDepth, depth);
 
   const sideRight = new Mesh(sideGeometry, defaultMaterial());
   const sideLeft = new Mesh(sideGeometry, defaultMaterial());
   const sideBack = new Mesh(sideBackGeometry, defaultMaterial());
-  const sideTopFront = new Mesh(sideTopGeometry, defaultMaterial());
-  const sideTopBack = new Mesh(sideTopGeometry, defaultMaterial());
   const shelf = new Mesh(shelfGeometry, defaultMaterial())
 
   caseGroup.add(sideRight)
   caseGroup.add(sideLeft)
   caseGroup.add(sideBack)
-  caseGroup.add(sideTopFront)
-  caseGroup.add(sideTopBack)
+  caseGroup.add(beams)
   caseGroup.add(shelf)
 
   sideRight.position.set(width / 2 - sideDepth /2, sideY, 0)
   sideLeft.position.set(-width / 2 + sideDepth /2, sideY, 0)
   sideBack.position.set(0, sideY, - depth / 2 + sideDepth)
-  sideTopFront.position.set(0, height - legsHeight - sideDepth * 2, depth / 2 - sideTop / 2)
-  sideTopBack.position.set(0, height - legsHeight - sideDepth * 2, -depth / 2 + sideTop / 2)
+
   shelf.position.set(0, sideY, 0)
 
+  /*
   const facadeGroup = new Group()
   facadeGroup.position.set(-width / 2 - sideDepth /2, sideY, depth / 2)
   const facadeGeometry = new BoxGeometry(width - sideDepth /4, height - legsHeight, sideDepth )
@@ -57,10 +56,10 @@ export const f_400 = () => {
   facadeGroup.visible = true
   facadeGroup.add(facade)
   caseGroup.add(facadeGroup)
-
+*/
   boxGroup.name = 'f_400'
   boxGroup.userData['code'] = 'f-400'
-  boxGroup.userData['facadeVariants'] = ['397_716_0_solid_1']
+  //boxGroup.userData['facadeVariants'] = ['397_716_0_solid_1']
   boxGroup.userData['configType'] = 'boxFloor'
   boxGroup.userData['openedDoors'] = false
 
