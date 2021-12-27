fa4f22b387d308121b76845c2f32f8a70f453a56
Heinrich Schmidt <littlepervert@yandex.ru>
2021-12-27 11:44:36 +0300

    Освещение

--- components/constructor3d/configs/LightConfig.js
+++ components/constructor3d/configs/LightConfig.js
@@ -19,25 +19,34 @@ const spotLight_1 = () => {
 }
 
 const spotLight_2 = () => {
-  const spotLight = new PointLight(0xffffff);
-  spotLight.position.set(-80, 10, 5);
-  spotLight.intensity = 0
+  const spotLight = new SpotLight(0xffffff);
+  spotLight.target.position.set(-1, 55, 1);
+  spotLight.position.set(-20, -55, 20);
+  spotLight.intensity = 0.3
   return spotLight
 }
 
 const spotLight_3 = () => {
   const spotLight = new PointLight(0xffffff);
-  spotLight.position.set(-5, 10, 80);
-  spotLight.intensity = 0
+  spotLight.position.set(-30, 10, -1);
+  spotLight.intensity = 0.2
   return spotLight
 }
 
-
+ 
 const spotLight_4 = () => {
-  const spotLight = new SpotLight(0xffffff);
-  spotLight.position.set(0, 0, 0);
-  spotLight.target.position.set(-60 * n, 55 * n, 60 * n);
-  spotLight.intensity = 0
+  const spotLight = new PointLight(0xffffff);
+  spotLight.position.set(-5, 10, 80);
+  //spotLight.target.position.set(-60 * n, 55 * n, 60 * n);
+  spotLight.intensity = 0.16
+  return spotLight
+}
+
+const spotLight_5 = () => {
+  const spotLight = new PointLight(0xffffff);
+  spotLight.position.set(-1, 17, 30);
+  //spotLight.target.position.set(-60 * n, 55 * n, 60 * n);
+  spotLight.intensity = 0.2
   return spotLight
 }
 
@@ -46,7 +55,8 @@ const spotLights = [
   spotLight_1(),
   spotLight_2(),
   spotLight_3(),
-  spotLight_4()
+  //spotLight_4(),
+  //spotLight_5()
 ]
 
 export default {
